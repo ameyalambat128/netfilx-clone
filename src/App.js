f19901635da60cc79b257a26c3d3aa8f8a4c7f83
Ameya Lambat <ameyalambat@gmail.com>
2022-05-17 18:28:48 -0400

    feat: added a startup screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
+import LoginScreen from "./components/LoginScreen/LoginScreen";
 
 function App() {
+  const user = false;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <BrowserRouter>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzozPW-A5RSU04tU23Wr44elxjCDC78TQ",
+  authDomain: "netflix-clone-a5295.firebaseapp.com",
+  projectId: "netflix-clone-a5295",
+  storageBucket: "netflix-clone-a5295.appspot.com",
+  messagingSenderId: "415393826770",
+  appId: "1:415393826770:web:b5cd51ca048239c94c6a0b",
+};
